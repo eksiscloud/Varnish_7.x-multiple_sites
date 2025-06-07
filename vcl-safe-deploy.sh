220413507dc488a5e3e6dd959d884b64058cd766
eksiscloud <jakke.lehtonen@gmail.com>
2025-06-08 02:08:35 +0300

    Fix after Varnish upgrade

--- vcl-safe-deploy.sh
+++ vcl-safe-deploy.sh
@@ -31,8 +31,8 @@ for site in "${sites[@]}"; do
     vclname="${site}_${timestamp}"
     echo "==> Update $site → $vclname"
 
-    if varnishadm vcl.load "$vclname" "$vclfile"; then
-        varnishadm vcl.label "$site" "$vclname"
+    if varnishadm -n varnishd vcl.load "$vclname" "$vclfile"; then
+        varnishadm -n varnishd vcl.label "$site" "$vclname"
         echo "✓ Label has set: $site → $vclname"
     else
         echo "✗ ERROR: VCL loading failed for $site"
