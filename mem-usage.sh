30faf96d2a17dbb6e5971289c2720834909c9f0d
eksiscloud <jakke.lehtonen@gmail.com>
2025-06-22 22:34:23 +0300

    Use right formula

--- mem-usage.sh
+++ mem-usage.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
 
-# Collecting data
-used_bytes=$(varnishstat -1 -f SMA.s0.c_bytes | awk '{print $2}')
-garbage_bytes=$(varnishstat -1 -f SMA.s0.g_bytes | awk '{print $2}')
-object_count=$(varnishstat -1 -f SMA.s0.c_objects 2>/dev/null | awk '{print $2}')
+# Hae ajantasaiset muistiarvot
+current_bytes=$(varnishstat -1 -f SMA.s0.g_bytes | awk '{print $2}')
+available_bytes=$(varnishstat -1 -f SMA.s0.g_space | awk '{print $2}')
 
-# Total usage
-total_bytes=$(echo "$used_bytes + $garbage_bytes" | bc)
+# Lasketaan kokonaisvarattu muistialue (käytetty + vapaa)
+total_bytes=$(echo "$current_bytes + $available_bytes" | bc)
 
-# Human readable GB
-used_gb=$(echo "scale=2; $used_bytes / 1024 / 1024 / 1024" | bc)
-garbage_gb=$(echo "scale=2; $garbage_bytes / 1024 / 1024 / 1024" | bc)
+# Muunnetaan GiB-yksikköihin
+current_gb=$(echo "scale=2; $current_bytes / 1024 / 1024 / 1024" | bc)
+available_gb=$(echo "scale=2; $available_bytes / 1024 / 1024 / 1024" | bc)
 total_gb=$(echo "scale=2; $total_bytes / 1024 / 1024 / 1024" | bc)
 
-# Counting garbage share
-garbage_percent=$(echo "scale=1; 100 * $garbage_bytes / $total_bytes" | bc)
+# Lasketaan käyttöaste prosentteina
+usage_percent=$(echo "scale=1; 100 * $current_bytes / $total_bytes" | bc)
 
-# Printing
+# Haetaan objektien määrä (jos saatavilla)
+object_count=$(varnishstat -1 -f SMA.s0.g_alloc 2>/dev/null | awk '{print $2}')
+
+# Tulostetaan yhteenveto
 echo
-echo "Varnish Cache - memory usage:"
+echo "Varnish Cache - memory usage (malloc backend):"
 echo "-----------------------------------------------"
-echo "Memory in use:             ${used_gb} GiB"
-echo "Marked as garbage:         ${garbage_gb} GiB (${garbage_percent}% of memory)"
-echo "Totally in use:            ${total_gb} GiB"
+echo "Memory in use:             ${current_gb} GiB (${usage_percent} % of pool)"
+echo "Memory still available:    ${available_gb} GiB"
+echo "Total malloc pool size:    ${total_gb} GiB"
 
-#if [[ -n "$object_count" ]]; then
-#    echo "Amount of objects in cache: $object_count"
-#else
-#    echo "Amount of objects isn't available (not supporter or no data)."
-#fi
+if [[ -n "$object_count" ]]; then
+    echo "Currently cached objects:  $object_count"
+else
+    echo "Object count is not available."
+fi
