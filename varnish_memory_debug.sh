54fdce6c5080ef9d87a81ea2a150150e15f1f93b
eksiscloud <jakke.lehtonen@gmail.com>
2025-07-06 09:57:18 +0300

    Translate

--- varnish_memory_debug.sh
+++ varnish_memory_debug.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 #
-# Varnish-muistin pudotuksen debug-skripti
-# Tallentaa järjestelmän tilanteen, jos Varnishin muistikulutus romahtaa
+# Amount of RAM went to zero every now and then.
+# This logs some metrics.
 #
-# Suositeltu ajastus esim. 5 minuutin välein crontabissa
+# Use crontab, i.e. every 5 minutes.
+#
+# the guilty one of was one unactive cache purging plugin in one WordPress.
+# When using wget to build up snapshot-backend, it sended BAN for everything.
 
 LOGDIR="/var/log/varnish_memory_watch"
 mkdir -p "$LOGDIR"
@@ -14,7 +17,7 @@ echo "=== Varnish Memory Debug ===" > "$LOGFILE"
 echo "Timestamp: $NOW" >> "$LOGFILE"
 echo "" >> "$LOGFILE"
 
-# Varnish PID ja muistitiedot
+# Varnish PID and memory
 VARNISHPID=$(pidof varnishd)
 echo ">> /proc/$VARNISHPID/status:" >> "$LOGFILE"
 grep -E 'VmRSS|VmSize|VmData' /proc/$VARNISHPID/status >> "$LOGFILE"
@@ -25,12 +28,12 @@ echo ">> varnishstat (selected fields):" >> "$LOGFILE"
 varnishstat -1 -f SMA.s0.c_bytes,SMA.s0.g_bytes,SMA.s0.c_objects,SMA.s0.g_space,MAIN.n_expired,MAIN.n_lru_nuked >> "$LOGFILE" 2>/dev/null
 echo "" >> "$LOGFILE"
 
-# Kernel logista mahdollisia muistiviitteitä
+# Kernel log
 echo ">> Kernel/syslog memory-related messages (past 5 minutes):" >> "$LOGFILE"
 journalctl --since "5 min ago" | grep -Ei 'oom|kill|memory|swap|invoked' >> "$LOGFILE"
 echo "" >> "$LOGFILE"
 
-# dmesg mahdolliset viimeisimmät viestit
+# dmesg last messages
 echo ">> dmesg -T | tail -n 20:" >> "$LOGFILE"
 dmesg -T | tail -n 20 >> "$LOGFILE"
 
