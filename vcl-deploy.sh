fb2e16cc5890ab413a8c840fc31d33814b61e8ba
eksiscloud <jakke.lehtonen@gmail.com>
2025-08-05 01:29:40 +0300

    Fix vcl path

--- vcl-deploy.sh
+++ vcl-deploy.sh
@@ -7,12 +7,12 @@ timestamp=$(date +"%Y%m%d_%H%M%S")
 
 # Sites and vcls, must ne same load than in start.cli
 declare -A vclmap=(
-    [poochie]="/etc/varnish/sites/poochierevival.vcl"
-    [dev]="/etc/varnish/sites/dev.eksis.vcl"
-    [jagster]="/etc/varnish/sites/jagster.eksis.vcl"
-    [eksis]="/etc/varnish/sites/eksis.one.vcl"
-    [selko]="/etc/varnish/sites/selko.katiska.vcl"
-    [katiska]="/etc/varnish/sites/katiska.eu.vcl"
+    [poochie]="/etc/varnish/sites/shared_wp.vcl"
+    [dev]="/etc/varnish/sites/shared_wp.vcl"
+    [jagster]="/etc/varnish/sites/shared_wp.vcl"
+    [eksis]="/etc/varnish/sites/shared_wp.vcl"
+    [selko]="/etc/varnish/sites/shared_wp.vcl"
+    [katiska]="/etc/varnish/sites/shared_wp.vcl"
 )
 
 for site in "${!vclmap[@]}"; do
