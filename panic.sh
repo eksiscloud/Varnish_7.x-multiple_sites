e062d7d028d7e066ac1e7deb573c027bbb0025ae
eksiscloud <jakke.lehtonen@gmail.com>
2025-09-02 22:06:54 +0300

    Add comment

--- panic.sh
+++ panic.sh
@@ -1,4 +1,5 @@
-#! /bin/bash
+# Manual panicking, rarely needed anymore. I automated this. Check out /scripts
 
-varnishd -I /etc/varnish/start.cli.emerg -P /var/run/varnish.pid -j unix,user=vcache -F -a :8080 -T localhost:6082 -f "" -S /etc/varnish/secret -s malloc,256M
-
+varnishd -I /etc/varnish/start.cli.emerg -P /var/run/varnish.pid \
+  -j unix,user=vcache -F -a :8080 -T localhost:6082 -f "" \
+  -S /etc/varnish/secret -s malloc,256M
